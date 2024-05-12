052c5b4950cd124f2dafa765c6c094105cd362d6
Duy Nguyen <duynguyen1332000@gmail.com>
2024-05-12 16:38:03 +0700

    dashboard - done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,6 +21,26 @@ class DashboardController extends Controller
             'orders.ten_khach_hang as customer_name'
         )->orderBy('orders.created_at')->get();
 
+        $recent_invoice = DB::table('orders')->whereNotNull('total_price')->select(
+            'orders.id as id',
+            'orders.total_price as total_price',
+            'orders.created_at as created_at',
+            'orders.ten_khach_hang as customer_name'
+        )->orderBy('orders.created_at')->get();
+
+        $top_revenue_table = DB::table('orders')
+                ->groupBy('table_id')
+                ->join('tables', 'tables.id', '=', 'orders.table_id')
+                ->join('setting_table', 'setting_table.id', '=', 'tables.setting_table_id')
+                ->select(
+                    'tables.id as table_id',
+                    'tables.name as table_name',
+                    DB::raw('SUM(total_price) as total_revenue'),
+                    'setting_table.type as setting_table_type'
+                )
+                ->orderByDesc('total_revenue')
+                ->get();
+
         return response()->json(
             [
                 'message' => 'Successfully',
@@ -29,7 +49,8 @@ class DashboardController extends Controller
                     "total_invoice" => $total_invoice,
                     "total_employee" => $total_employee,
                     "total_product" => $total_product,
-                    "recent_invoice" => $recent_invoice
+                    "recent_invoice" => $recent_invoice,
+                    "top_revenue_table" => $top_revenue_table
                 ]
             ]
         );
